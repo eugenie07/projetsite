15167f7ec5ce0e5d6945866ab7ff071677b2463c
eugenie <cocheteau.e@gmail.com>
2018-07-24 12:07:21 +0200

    commit 10
    jquery

--- projetsite.js
+++ projetsite.js
@@ -1,5 +1,27 @@
 
 $(".art div").click(function () {
-  $(this).addClass('mot1');
+  $(this).addClass('mot1')
 
 });
+
+/*
+$(function(){
+  $('h3').hide();
+    if($('h3').mousemove()){
+      $('h3').fadeIn(3000);
+    }else{
+      $('h3').hide();
+    }
+});
+*/
+
+var mouseX, mouseY;
+$(document).mousemove(function(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    var opacity = (mouseY / 400);
+    opacity = opacity > 1 ? 1 : opacity;
+     $('h3').css("opacity", opacity);
+     sss
+    console.log("mouse position logged at pos X" + mouseX + " Y " + mouseY);
+}).mouseover();
