96967fdb687822212e37efc7317cb6b17b64c52b
eugenie <cocheteau.e@gmail.com>
2018-07-26 17:02:37 +0200

    commit12
    page dessins
    aquarelle

--- projetsite.js
+++ projetsite.js
@@ -4,16 +4,6 @@ $(".art div").click(function () {
 
 });
 
-/*
-$(function(){
-  $('h3').hide();
-    if($('h3').mousemove()){
-      $('h3').fadeIn(3000);
-    }else{
-      $('h3').hide();
-    }
-});
-*/
 
 var mouseX, mouseY, opacity = 0;
 $(document).mousemove(function(e) {
@@ -28,22 +18,28 @@ $(document).mousemove(function(e) {
 }).mouseover();
 
 
+
+var array = ['IMG_20180216_112811_204.jpg','IMG_20170901_135424_006.jpg','IMG_20170417_153919_279.jpg','dessin .jpg']
+var compteur = 0
+$(".bouton").click(function(){
+  compteur = compteur + 1
+  if (compteur == 4){
+    compteur = 0
+  }
+  $('#img').attr('src',array[compteur])
+
+});
+
 /*
-var mouseX, mouseY, width = 300, height = 300;
-$(document).mousemove(function(e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-    if (height < 400)
-    {
-      height = (mouseY, mouseX / 400);
-      height = height > 1 ? 1: height;
-      $('.art div').css("height", height);
-    }
-    if (width < 400)
-    {
-      width = (mouseY, mouseX / 400);
-      width = width > 1 ? 1 : width;
-      $('.art div').css("width", width);
-    }
-}).mouseover();
+function imageGallerie()
+{
+  var active = $('#gallerie.active');
+  var next = (active.next().lengh > 0) ? active.next() : $('gallerie img:first');
+      active.fadeOut(function(){
+      active.removeClass('active');
+      next.fadeIn().addClass('active');
+    });
+}
+
+setInterval('imageGallerie()',2500);
 */
